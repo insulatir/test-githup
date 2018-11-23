cade747cd5a0f4c31b966d5d4b3739768771edb6
bluesocks <s42415466@gmail.com>
2018-11-23 19:39:10 +0900

    sum() 추가

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,6 @@
 public class Calculator {
-  public static void main(String[] args) {
-    public Calculator() {
-
-    }
-
     public int add(int a, int b) {
       return a + b;
-    } 
-  }
+    }
+    
 }
