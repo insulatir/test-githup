e9436ebaf9cfbf78a664d250a33e6b46a14ef814
bluesocks <s42415466@gmail.com>
2018-11-23 20:31:30 +0900

    main 추가

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,11 @@
 public class Calculator {
-    public int add(int a, int b) {
-      return a + b;
-    }
+    public static void main(String[] args) {
+      public int add(int a, int b) {
+        return a + b;
+      }
 
-    public int sub(int a, int b) {
-      return a - b;
+      public int sub(int a, int b) {
+        return a - b;
+      }
     }
 }
